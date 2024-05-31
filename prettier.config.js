afab74ba7f120463aac1fcaabc6171decbea77e4
logantai24 <logantai24@gmail.com>
2024-05-31 08:07:57 +0000

    feat: add lint and slight structure update

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  printWidth: 100,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-tailwindcss']
+};
